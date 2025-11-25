01c9b9e0ace3280810dec0896910d67c802bf876
raniel-roque <raniel.roque0509@gmail.com>
2025-11-25 11:59:56 +0800

    Finished Day 16

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,33 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+
 Route::get('/jobs', function() {
-    return view('jobs', [
-        'jobs'=> Job::with('employer')->simplePaginate(20)
+    return view('jobs.index', [
+        'jobs'=> Job::with('employer')->latest()->simplePaginate(4)
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
 
+Route::post('/jobs', function () {
+    //validation
+
+    Job::create([
+        'title'=> request('title'),
+        'salary'=> request('salary'),
+        'employer_id'=> 1,
+    ]);
+
+    return redirect('/jobs');
+});
 
 Route::get('/contact', function () {
     return view('contact');
